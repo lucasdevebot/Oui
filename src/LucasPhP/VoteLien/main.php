c3b8f0134153e9eac3a7bab72038f7c9fc14080a
lucasdevebot <68424725+lucasdevebot@users.noreply.github.com>
2020-12-22 21:43:16 +0100

    Add files via upload

---
+++ src/LucasPhP/VoteLien/main.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace LucasPhP\VoteLien;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+
+
+class main extends PluginBase{
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
+    {
+        $commandName = $command->getName(); //Récuperation dans une variable du nom de la commande
+        if($sender instanceof Player){ //si sender= l'envoyeur de la commande est un Player psq la console ne peut pas avoir de perm
+            if(strtolower($commandName) === 'votelien') { //strtolower = mettre la commande en minuscule
+                $sender->sendMessage('§1Tu peux voter en allant sur §ahttps://minecraftpocket-servers.com/server/105922/ §1puis en faisant §a/vote §1pour récupérer tes récompenses  ! ');
+                return true;
+            }
+        }
+        return true;
+    }
+}
